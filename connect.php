510af16bdd6831a1d31e702137c60d8b8f01fb65
gcl <737720583@qq.com>
2019-11-24 22:11:10 +0800

    fixed the broken email address

--- connect.php
+++ connect.php
@@ -1,17 +1,1 @@
-<?php 
-session_start();
-//connect.php
-$server	    = 'localhost';
-$username	= 'evert_padje';
-$password	= 'superdatabase';
-$database	= 'evertsmitnet_main';
-
-if(!mysql_connect($server, $username, $password))
-{
- 	exit('Error: could not establish database connection');
-}
-if(!mysql_select_db($database))
-{
- 	exit('Error: could not select the database');
-}
-?>
+'fixed the broken email address'
